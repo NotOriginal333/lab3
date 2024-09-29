fb2b3de662501a00b26895c615cee76cef71a787
NotOriginal333 <m.naumovych2@gmail.com>
2024-09-29 19:52:56 +0300

    fixed imports

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,6 +1,10 @@
 package task1;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
